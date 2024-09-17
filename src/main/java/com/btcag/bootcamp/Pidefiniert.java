72bfb0a29ee7c4bbada73d617fcda1b7bdb208b9
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-17 17:09:02 +0200

    pidefiniert entwurf

---
+++ src/main/java/com/btcag/bootcamp/Pidefiniert.java
@@ -0,0 +1,22 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class Pidefiniert {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wie viele Iterationen sollen gemacht werden um Pi zu berechnen?");
+        long i = scanner.nextLong();
+        double pi = 4.0;
+        int k = 0;
+
+        while (k <= i){
+            pi = 4 * (1 - );  // Modulo benutzen um gerade und ungerade zu wechseln um +/- zu wechseln
+            k++;
+
+        }
+
+        System.out.println("Nach " + i + "iterationen wurde Pi  auf den Wert " + pi + "berechnet.");
+
+    }
+}
