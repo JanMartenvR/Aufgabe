5da86f8038536484abab0ade1cde79dabfb9bec4
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-17 15:16:40 +0200

    arithmetischeOperation push2

--- src/main/java/com/btcag/bootcamp/arithmetischeOperation.java
+++ src/main/java/com/btcag/bootcamp/arithmetischeOperation.java
@@ -4,6 +4,7 @@ public class arithmetischeOperation {
     public static void main(String[] args) {
         int test = 10;
         System.out.println("" + test++);
+        System.out.println("" + test);
         test = 10;
         System.out.println("" + ++test);
     }
---
+++ src/main/java/com/btcag/bootcamp/rueckwaertsAusgeben.java
@@ -0,0 +1,23 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class rueckwaertsAusgeben {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gebe eine postive ganze Zahl ein:");
+        String zahl = scanner.nextLine();
+        String result = "";
+        int length = zahl.length();
+
+        while (length >= 0){
+            result = result + zahl.charAt(length-1);
+            length--;
+        }
+
+        System.out.println(""+result);
+
+
+
+    }
+}
