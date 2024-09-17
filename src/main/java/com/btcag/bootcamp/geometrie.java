7c8121bee2bae7950f21d7ff052e73417716d0c7
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-17 16:38:21 +0200

    geometrie entwurf 1

---
+++ src/main/java/com/btcag/bootcamp/geometrie.java
@@ -0,0 +1,24 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class geometrie {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte ganze Zahl größer als 0 eingeben :");
+        int r = scanner.nextInt();
+        double area = 2 * Math.PI * r*r;
+
+        System.out.println(""+area);
+
+        System.out.println("Bitte eine Seitenlänge eingeben:");
+        int x = scanner.nextInt();
+        System.out.println("Bitte andere Seitenlänge eingeben:");
+        int y = scanner.nextInt();
+
+        double area_square = x * y;
+
+        System.out.println(""+area_square);
+
+    }
+}
