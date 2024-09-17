7ad8756c3ac29e08155667a3b39a14d692cd8248
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-17 14:51:51 +0200

    arithmetischeOperation push

---
+++ src/main/java/com/btcag/bootcamp/arithmetischeOperation.java
@@ -0,0 +1,10 @@
+package com.btcag.bootcamp;
+
+public class arithmetischeOperation {
+    public static void main(String[] args) {
+        int test = 10;
+        System.out.println("" + test++);
+        test = 10;
+        System.out.println("" + ++test);
+    }
+}
