f7f15152a47ed16604dbb300280a76671d0f1115
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-17 16:24:33 +0200

    Quersumme final

--- src/main/java/com/btcag/bootcamp/Quersumme.java
+++ src/main/java/com/btcag/bootcamp/Quersumme.java
@@ -1,7 +1,35 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class Quersumme {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte Zahl eingeben (String):");
+        String zahl = scanner.nextLine();
+        int length = zahl.length();
+        int summe = 0;
+
+        while (length > 0){
+            char c = zahl.charAt(length-1);
+            length--;
+            int x = c - 48;
+            summe = summe + x;
+        }
+        System.out.println(""+summe);
+
+
+        System.out.println("Bitte Zahl eingeben (Long):");
+        long zahl_int = scanner.nextLong();
+        long result_int = 0;
+        while (zahl_int != 0){
+            result_int = result_int + zahl_int % 10;
+            zahl_int /= 10;
+        }
+
+        System.out.println(""+result_int);
+
+
 
     }
 }
