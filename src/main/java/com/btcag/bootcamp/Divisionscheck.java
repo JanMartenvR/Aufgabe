410225190305d36f9ea38724a346fc7e473bbb50
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-17 16:57:29 +0200

    divisionscheck fertig

---
+++ src/main/java/com/btcag/bootcamp/Divisionscheck.java
@@ -0,0 +1,21 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class Divisionscheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcher Divisor soll geprüft werden?");
+        int div = scanner.nextInt();
+        System.out.println("Bis zu welcher Zahl?");
+        int max = scanner.nextInt();
+        int i = 0;
+        int ausgabe = 0;
+        while (ausgabe < (max - div)){
+            ausgabe = div * i;
+            i++;
+            System.out.println(""+ausgabe);
+        }
+
+    }
+}
