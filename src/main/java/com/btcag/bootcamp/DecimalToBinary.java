c7e81d8ceb8782ff0fe057ee4d48828baaee462d
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-17 14:01:50 +0200

    Neuer Versuch2

--- src/main/java/com/btcag/bootcamp/DecimalToBinary.java
+++ src/main/java/com/btcag/bootcamp/DecimalToBinary.java
@@ -1,4 +1,20 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gebe eine postive ganze Zahl ein:");
+        int decimal = scanner.nextInt();
+        String result = "";
+
+        while (decimal > 0){
+            int resultModulo = decimal % 2;
+            result = resultModulo + result;
+            decimal = decimal / 2 ;
+
+        }
+        System.out.print("Das Ergebnis lautet:" + result);
+    }
 }
--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -1,7 +1,11 @@
 package com.btcag.bootcamp;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bitte Zahl eingeben zur Quadratzahlberechnung");
+        Scanner scanner = new Scanner(System.in);
+        long thenumber = scanner.nextLong();
+        thenumber *= thenumber;
+        System.out.println("Ergebnis:" + thenumber);
     }
 }
