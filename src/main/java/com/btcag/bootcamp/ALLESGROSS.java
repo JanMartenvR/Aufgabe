d6dfe3bf89d7e0db1379dbce7cc6b59569cfbc42
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-18 11:01:33 +0200

    ALLESGROSS funktioniert

--- src/main/java/com/btcag/bootcamp/ALLESGROSS.java
+++ src/main/java/com/btcag/bootcamp/ALLESGROSS.java
@@ -7,15 +7,16 @@ public class ALLESGROSS {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Gebe ein Wort ein:");
         String wort = scanner.nextLine();
-        long length = wort.length();
-        int ASCII = 0;                          //-20
-        while (length > 0){
-            char c = wort.charAt(length-1);
+        int length = wort.length();
+        int ASCII = 0;
+        int i = 0;
+        while (length > i){
+            char c = wort.charAt(0+i);
             ASCII = c;
-            ASCII = ASCII - 20;
+            ASCII = ASCII - 32;
             char out = (char) ASCII;
             System.out.print(out+"");
-            length++;
+            i++;
         }
 
 
