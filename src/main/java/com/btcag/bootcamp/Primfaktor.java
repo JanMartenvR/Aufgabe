d8bf5922e3873338a80ff3f134f9d4cdbe308339
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-19 10:44:41 +0200

    Primfaktor verwirrung

--- src/main/java/com/btcag/bootcamp/Primfaktor.java
+++ src/main/java/com/btcag/bootcamp/Primfaktor.java
@@ -1,7 +1,19 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class Primfaktor {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte Zahl für den Primetest eingeben (Long):");
+        long zahl = scanner.nextLong();
+        int faktor = 0;
+        long i = 2;
+
+        // Erst Test ob Primzahl,  dann ob durch folgende primzahlen ohne rest teilbar
+
+        System.out.println("Ergebnis: ");
+
 
     }
 }
