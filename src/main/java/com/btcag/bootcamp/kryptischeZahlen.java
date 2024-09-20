df4389e0e3f1c5979b3f73639f7814f8e3bfb1a8
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-20 08:20:03 +0200

    Rahmen kryptischeZahlen

--- src/main/java/com/btcag/bootcamp/kryptischeZahlen.java
+++ src/main/java/com/btcag/bootcamp/kryptischeZahlen.java
@@ -10,10 +10,13 @@ public class kryptischeZahlen {
         System.out.println("Bitte Passwort (ZAHL) eingeben:");
         long passwort = scanner.nextLong();
         long result = 0;
+        long input_var = input;
 
-        while (){
-
+        while (input_var != 0){
+            result = input_var % 10;
+            input_var /= 10;
         }
+
         while (passwort == input) {
             result = input;
         }
