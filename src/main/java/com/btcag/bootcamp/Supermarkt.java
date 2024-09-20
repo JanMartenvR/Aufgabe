eb7a8284c2a076aa6f59bc2190a9388102fcb512
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-20 13:14:29 +0200

    Supermarkt output vorbereitet

--- src/main/java/com/btcag/bootcamp/Supermarkt.java
+++ src/main/java/com/btcag/bootcamp/Supermarkt.java
@@ -15,5 +15,24 @@ public class Supermarkt {
         int euro = summe / 100;
         int cent = summe % 100;
         System.out.println("Die Summe aller Produkte ist " + euro + " € und " + cent + " Cent.");
+
+        System.out.println("Wieviel hat der Kunde gegeben (in Cent)? ");
+        int bezahlt = scanner.nextInt();
+
+        System.out.println("Bitte folgendes Geld zurückgeben (" + rückgeld + ")");
+        System.out.println(zwh + " x 200 €");
+        System.out.println(eih + " x 100 €");
+        System.out.println(fuenfzige + " x 50 €");
+        System.out.println(zwanzige + " x 20 €");
+        System.out.println(zehne + " x 10 €");
+        System.out.println(fuenfe + " x 5 €");
+        System.out.println(zweie + " x 2 €");
+        System.out.println(eine + " x 1 €");
+        System.out.println(fuenfzigc + " x 50 Cent");
+        System.out.println(zwanzigc + " x 20 Cent");
+        System.out.println(zehnc + " x 10 Cent");
+        System.out.println(fuenfc + " x 5 Cent");
+        System.out.println(zweic + " x 2 Cent");
+        System.out.println(einc + " x 1 Cent");
     }
 }
