37775931a895f1f36b05a1002d12547d92953876
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-23 13:34:42 +0200

    Initialisierung Zahlenraten

---
+++ src/main/java/com/btcag/bootcamp/Zahlenraten.java
@@ -0,0 +1,12 @@
+package com.btcag.bootcamp;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Zahlenraten {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
