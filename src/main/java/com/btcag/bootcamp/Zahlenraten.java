d6493625ff289468e52aa9b648bc68ddd03ad043
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-23 13:54:54 +0200

    Zahlenraten Biteinstellung und Raten implementiert. Größer oder kleiner Hilfe fehlt.

--- src/main/java/com/btcag/bootcamp/Zahlenraten.java
+++ src/main/java/com/btcag/bootcamp/Zahlenraten.java
@@ -7,6 +7,53 @@ public class Zahlenraten {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte gebe die Zahlengröße in Bits an:");
+        int bits = scanner.nextInt();
+        int number = random.nextInt(bits);
+        int input = 1000000;
+        int x = 0;
+        int y = 0;
+        while (bits == 8){
+            y = 255;
+            bits++;
+        }
+        while (bits == 7){
+            y = 127;
+            bits++;
+        }
+        while (bits == 6){
+            y = 63;
+            bits++;
+        }
+        while (bits == 5){
+            y = 31;
+            bits++;
+        }
+        while (bits == 4){
+            y = 15;
+            bits++;
+        }
+        while (bits == 3){
+            y = 7;
+            bits++;
+        }
+        while (bits == 2){
+            y = 3;
+            bits++;
+        }
+        while (bits == 1){
+            y = 1;
+            bits++;
+        }
+
+        int i = 1;
+        while (input != number) {
+
+            System.out.println(i + ". Versuch: Bitte eine Zahl im Bereich von " + x + " bis " + y+ " eingeben:");
+            input = scanner.nextInt();
+            i++;
+
+        }
 
     }
 }
