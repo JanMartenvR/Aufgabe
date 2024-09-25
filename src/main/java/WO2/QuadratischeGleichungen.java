93ba2fd3be89340ce6ad9e3fb02ce3802337868e
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-25 13:23:57 +0200

    Lösung über diskrimante implementiert.

--- src/main/java/WO2/QuadratischeGleichungen.java
+++ src/main/java/WO2/QuadratischeGleichungen.java
@@ -5,14 +5,24 @@ import java.util.Scanner;
 public class QuadratischeGleichungen {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] zahlen = new int[3];
-        System.out.println("Bitte geben Sie drei Variablen der quadratischen Gleichung ein: ");
-        zahlen[0] = scanner.nextInt();
-        zahlen[1] = scanner.nextInt();
-        zahlen[2] = scanner.nextInt();
+        float[] zahlen = new float[3];
+        System.out.println("Bitte geben Sie drei Variablen der quadratischen Gleichung ein ax^2+bx+c: ");
+        zahlen[0] = scanner.nextFloat();
+        zahlen[1] = scanner.nextFloat();
+        zahlen[2] = scanner.nextFloat();
 
-        for (int i = 0; i < zahlen.length; i++){
-            System.out.println(zahlen[i]);
+        int loesungen = 0;
+        float diskriminante = zahlen[1] * zahlen[1] - 4 * zahlen[0] * zahlen[2];
+
+        if (diskriminante > 0) {
+            loesungen = 2;
+        } else if (diskriminante == 0) {
+            loesungen = 1;
+
+        } else {
+            loesungen = 0;
         }
+        System.out.println("Die Gleichung hat " + loesungen + " reelle Lösungen.");
+
     }
 }
