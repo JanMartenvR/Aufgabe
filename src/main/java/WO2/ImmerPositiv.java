b78c601cb89de44693c4204251911816e2109dc8
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-25 12:00:24 +0200

    Konzept erster Aufgabe

--- src/main/java/WO2/ImmerPositiv.java
+++ src/main/java/WO2/ImmerPositiv.java
@@ -1,7 +1,17 @@
 package WO2;
 
+import java.util.Scanner;
+
 public class ImmerPositiv {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte gebe eine Zahl ein: ");
+        int zahl = scanner.nextInt();
+
+        if (zahl < 0) {
+            zahl *= -1;
+        }
+        System.out.println(zahl);
 
     }
 }
