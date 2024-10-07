43d1472e03fa1948d9675fd25b67b9cac255fc47
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-10-07 15:00:31 +0200

    printBoard überarbeitet, sodass das Spielfeld richtig ausgegeben wird.

--- src/main/java/WO2/VierGewinnt.java
+++ src/main/java/WO2/VierGewinnt.java
@@ -66,12 +66,13 @@ public class VierGewinnt {
 
 
     public static void printBoard(int[][] board) {
-        for (int i = 0; i < board.length; i++) {
+        for (int i = board.length - 1; i >= 0; i--) {
             for (int j = 0; j < board[i].length; j++) {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
+
     }
 
     public static int[][] spielzug(int board[][], int player, int zug) {
