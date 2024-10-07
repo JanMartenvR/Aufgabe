99280bb6f33b5079c8728b141a4c77137649843e
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-10-07 11:26:24 +0200

    Arrays als globale Variable. Funktionen für die Nutzereingabe und Arrayinitialisierung hinzugefügt.

--- src/main/java/WO2/TuermeStapeln.java
+++ src/main/java/WO2/TuermeStapeln.java
@@ -3,21 +3,41 @@ package WO2;
 import java.util.Scanner;
 
 public class TuermeStapeln {
+    public static int[]stabEins;
+    public static int[]stabZwei;
+    public static int[]stabDrei;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Gebe die Höhe der Stapel ein:");
-        int hoehe = scanner.nextInt();
 
-        int[] stabEins = new int[hoehe];
-        int[] stabZwei = new int[hoehe];
-        int[] stabDrei = new int[hoehe];
+        int hoehe = nutzerEingabe();
+        arrayIntialisierung(hoehe);
 
-        for (int i = 0; i < hoehe; i++) {
-            stabEins[i] = i;
-        }
         for (int zahl : stabEins) {
             System.out.print(zahl +" ");
         }
+
+    }
+    public static int nutzerEingabe() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Gebe die Höhe der Stapel ein:");
+        int hoehe = scanner.nextInt();
+        return hoehe;
+    }
+    public static void arrayIntialisierung (int hoehe) {
+        stabEins = new int[hoehe];
+        stabZwei = new int[hoehe];
+        stabDrei = new int[hoehe];
+        for (int i = 0; i < hoehe; i++) {
+            stabEins[i] = i + 1;
+            stabZwei[i] = 0;
+            stabDrei[i] = 0;
+        }
+    }
+    public static void solve(int hoehe) {
+        if (hoehe == 1) {
+            System.out.println("Bewege Scheibe 1 von Turm 1 zur Turm 3.");
+            return;
+        }
+
 
     }
 }
