df7981650edb5bf38f649dd84b33f6b6166f02bd
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-10-09 15:19:32 +0200

    Initialisierung der Dateien und Interface von der QueueList

---
+++ src/main/java/queuelist/Array.java
@@ -0,0 +1,30 @@
+package queuelist;
+
+public class Array implements IQueueList{
+    int[] array;
+
+    @Override
+    public int popFront() {
+        return 0;
+    }
+
+    @Override
+    public int popLast() {
+        return 0;
+    }
+
+    @Override
+    public int pushLast(int i) {
+        return 0;
+    }
+
+    @Override
+    public int pushFront(int i) {
+        return 0;
+    }
+
+    @Override
+    public int get(int i) {
+        return 0;
+    }
+}
