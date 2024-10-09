bfe8a71965e641c8d0ad948bcb1f23949fbd7edb
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-10-09 15:32:27 +0200

    prototyp für popFront und popLast funktion eingefügt

--- src/main/java/queuelist/Array.java
+++ src/main/java/queuelist/Array.java
@@ -1,23 +1,29 @@
 package queuelist;
 
-public class Array implements IQueueList{
+public class Array implements IQueueList {
     int[] array;
 
     @Override
     public int popFront() {
-        int[]copy = new int[array.length-2];
+        int tmp = array[0];
+        int[] copy = new int[array.length - 2];
         for (int i = 0; i < copy.length - 1; i++) {
-            copy[i] = array[i+1];
+            copy[i] = array[i + 1];
         }
-        return array[0];
+        array = copy;
+        return tmp;
 
     }
 
     @Override
     public int popLast() {
-        int[]copy = array;
-        return array[array.length-1];
-
+        int tmp = array[array.length - 1];
+        int[] copy = new int[array.length - 2];
+        for (int i = 0; i < copy.length - 2; i++) {
+            copy[i] = array[i];
+        }
+        array = copy;
+        return tmp;
     }
 
     @Override
