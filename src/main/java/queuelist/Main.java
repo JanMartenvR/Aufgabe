c217ebfbb5dc67802b0c394dc2c2d532e63fe38e
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-10-10 11:39:34 +0200

    Objekt in main Methode erstellt

--- src/main/java/queuelist/Main.java
+++ src/main/java/queuelist/Main.java
@@ -2,7 +2,11 @@ package queuelist;
 
 public class Main {
     public static void main(String[] args) {
+        Array array = new Array();
+        array.array = new int[2];
 
+        int i = array.pushFront(1);
+        System.out.println(i);
 
     }
 }
